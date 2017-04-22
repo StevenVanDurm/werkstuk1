668bf62bf72f38dacacb89a5a90f2afd2b89da21
Steven <steven.van.durmestudent.ehb.be>
2017-04-23 01:39:04 +0200

    Initial Commit

---
+++ werkstuk1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  werkstuk1
+//
+//  Created by student on 23/04/2017.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
